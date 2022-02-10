1b42f88b7b6d9bbd94410570a6cef42b43f9c872
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-10 16:36:39 +0600

    Update ImagePicker.swift

--- Sources/ImagePicker/ImagePicker.swift
+++ Sources/ImagePicker/ImagePicker.swift
@@ -1,36 +1,39 @@
 import SwiftUI
 import UIKit
 
-public struct Camera: UIViewControllerRepresentable{
-    
+public struct ImagePicker{
     typealias pickedImageHandler = (UIImage?) -> Void
-    var handlePickedImage: pickedImageHandler
+    public init(){}
     
-    public func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.allowsEditing = true
-        picker.sourceType = .photoLibrary
-        picker.delegate = context.coordinator
-        
-        return picker
-    }
-    public func makeCoordinator() -> Coordinator {
-        Coordinator(handlePickedImage)
-    }
-    public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    public struct Camera: UIViewControllerRepresentable {
         var handlePickedImage: pickedImageHandler
         
-        init(_ handlePickedImage: @escaping pickedImageHandler){
-            self.handlePickedImage = handlePickedImage
+        public func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.allowsEditing = true
+            picker.sourceType = .photoLibrary
+            picker.delegate = context.coordinator
+            
+            return picker
         }
-        public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            handlePickedImage(info[.originalImage] as? UIImage)
+        public func makeCoordinator() -> Coordinator {
+            Coordinator(handlePickedImage)
         }
-        public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            handlePickedImage(nil)
+        public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+            var handlePickedImage: pickedImageHandler
+            
+            init(_ handlePickedImage: @escaping pickedImageHandler){
+                self.handlePickedImage = handlePickedImage
+            }
+            public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                handlePickedImage(info[.originalImage] as? UIImage)
+            }
+            public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                handlePickedImage(nil)
+            }
         }
-    }
-    public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
-        
+        public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+            
+        }
     }
 }
