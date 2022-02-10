e75c1dc38d174c8a19ab04f4cb58772fe7b7e41b
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-10 15:46:39 +0600

    Camera in swiftui

--- Sources/ImagePicker/ImagePicker.swift
+++ Sources/ImagePicker/ImagePicker.swift
@@ -1,6 +1,43 @@
+import SwiftUI
+import UIKit
 public struct ImagePicker {
-    public private(set) var text = "Hello, World!"
-
+  
     public init() {
     }
+    
+    struct Camera: UIViewControllerRepresentable{
+        typealias pickedImageHandler = (UIImage?) -> Void
+        var handlePickedImage: pickedImageHandler
+        
+        func makeUIViewController(context: Context) -> UIImagePickerController {
+            let picker = UIImagePickerController()
+            picker.allowsEditing = true
+            picker.sourceType = .photoLibrary
+            picker.delegate = context.coordinator
+            
+            return picker
+        }
+        func makeCoordinator() -> Coordinator {
+            Coordinator(handlePickedImage)
+        }
+        class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+            var handlePickedImage: pickedImageHandler
+            
+            init(_ handlePickedImage: @escaping pickedImageHandler){
+                self.handlePickedImage = handlePickedImage
+            }
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                handlePickedImage(info[.originalImage] as? UIImage)
+            }
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                handlePickedImage(nil)
+            }
+        }
+        func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+            
+        }
+        
+    }
+    
+
 }
